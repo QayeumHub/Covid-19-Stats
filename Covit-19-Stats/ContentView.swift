ecaa2b4f9d2fb4e00eef1a32b007efd557acf9a0
Abdul Meraj <webmaster@agdstudio.com>
2020-04-09 13:04:48 -0700

    Initial Commit

---
+++ Covit-19-Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covit-19-Stats
+//
+//  Created by Abdul Meraj on 4/9/20.
+//  Copyright © 2020 Abdul Meraj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
