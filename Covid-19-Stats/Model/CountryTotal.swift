fb5c4a51d32e1f0bcfeded960bc4e82026fa8d82
Abdul Meraj <webmaster@agdstudio.com>
2020-04-14 00:37:08 -0700

    -

--- Covid-19-Stats/Model/CountryTotal.swift
+++ Covid-19-Stats/Model/CountryTotal.swift
@@ -8,20 +8,42 @@
 
 import Foundation
 
+//struct CountryTotal: Identifiable, Decodable {
+//    var id:Int?
+//    var countryStats: [CountryStats]?
+//}
+//
+//struct CountryStats : Identifiable, Decodable {
+//    var id:Int?
+//    var city: String?
+//    var province: String?
+//    var country: String?
+//    var lastUpdate: String?
+//    var keyID: String?
+//    var confirmed: Int?
+//    var deaths: Int?
+//    var recovered: Int?
+//}
+
+
 struct CountryTotal: Identifiable, Decodable {
     var id:Int?
+    var country: Country?
+}
+
+// MARK: - DataClass
+struct Country: Identifiable, Decodable {
+    var id:Int?
+    var lastChecked: String?
     var countryStats: [CountryStats]?
 }
 
+// MARK: - Covid19Stat
 struct CountryStats : Identifiable, Decodable {
     var id:Int?
-    var city: String?
-    var province: String?
+    var city, province: String?
     var country: String?
     var lastUpdate: String?
     var keyID: String?
-    var confirmed: Int?
-    var deaths: Int?
-    var recovered: Int?
+    var confirmed, deaths, recovered: Int?
 }
-
